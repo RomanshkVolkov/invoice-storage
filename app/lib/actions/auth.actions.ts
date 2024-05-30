5a1db92cd3a383012b8ad01ac726ff522d4523b4
Diego <diegogutcat28@gmail.com>
2024-05-30 13:26:57 -0500

    installed nodemailer

--- app/lib/actions/auth.actions.ts
+++ app/lib/actions/auth.actions.ts
@@ -1,7 +1,25 @@
 'use server';
 
 import { AuthError } from 'next-auth';
+import { google } from 'googleapis';
+import nodemailer from 'nodemailer';
 import { signIn } from '@/auth';
+
+const CLIENT_ID =
+  '588536859750-62ehkc9vam0rqh2qm5tclrtnfigk1fp5.apps.googleusercontent.com';
+const CLIENT_SECRET = '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';
+const REDIRECT_URI =
+  '588536859750-62ehkc9vam0rqh2qm5tclrtnfigk1fp5.apps.googleusercontent.com';
+const REFRESH_TOKEN =
+  '1//04oSwX6szf1rGCgYIARAAGAQSNwF-L9IrTUHbseYD3v2LayUCGdLYFDmJGq2F90ZhxI0TASsBqXaHgp0TY4Jo8iWrB35Usiy905E';
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
 
 export async function authenticate(
   prevState: string | undefined,
@@ -13,6 +31,7 @@ export async function authenticate(
       password: formData.get('password'),
       redirectTo: '/dashboard',
     });
+    await sendEmail();
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
@@ -23,5 +42,38 @@ export async function authenticate(
       }
     }
     throw error;
+  }
+}
+
+export async function sendEmail() {
+  try {
+    const accessToken = await oAuth2Client.getAccessToken();
+    if (accessToken.token === null) {
+      throw new Error('Failed to retrieve access token');
+    }
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'diegogutcat28@gmail.com',
+        clientId: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        refreshToken: REFRESH_TOKEN,
+        accessToken: accessToken.token,
+      },
+    });
+
+    const mailOptions = {
+      from: 'diego.catzin@outlook.com',
+      to: 'diegogutcat28@gmail.com',
+      subject: 'Test Email with OAuth2',
+      text: 'Hello from Gmail using OAuth2!',
+    };
+
+    const result = await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully:', result);
+  } catch (error) {
+    console.error('Error sending email:', error);
   }
 }
